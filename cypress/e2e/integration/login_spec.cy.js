07d9c56e0b4585a60c83987c375b96a4d3813ab6
Karen Jo <karenfiksdahl@gmail.com>
2023-11-07 14:11:59 +0100

    installs eslint and prettier, updates eslintrc.json

--- cypress/e2e/integration/login_spec.cy.js
+++ cypress/e2e/integration/login_spec.cy.js
@@ -3,7 +3,11 @@ describe('Login', () => {
      //visit the login page
      cy.visit('https://karenjoo.github.io/social-media-client/');
      cy.get('[data-cy=loginButton]').click();
-        
+     
+      // Enter valid login credentials
+    cy.get('[data-cy=loginEmail]').type('test@noroff.no');
+    cy.get('[data-cy=loginPassword]').type('testPassword');
+
 
 
     })
