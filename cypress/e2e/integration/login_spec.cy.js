aad416efdcc4f446f30eb9f72b996b3e8c5c6478
Karen Jo <karenfiksdahl@gmail.com>
2023-11-10 16:05:10 +0100

    updates login button with .first

--- cypress/e2e/integration/login_spec.cy.js
+++ cypress/e2e/integration/login_spec.cy.js
@@ -3,14 +3,15 @@ describe('Login', () => {
       // Visit the login page
       cy.visit('./');
   
-      // Click the login button > .eq(0) to click the first btn 
-      cy.get('button[data-auth="login"]:visible').eq(0).click();
+      // Click the login button > .first to get the login btn
+      cy.get('button[data-auth="login"]:visible').first().click();
+      console.log('Clicked login button');
   
       // Wait for the modal to exist in the DOM
-      cy.get('#loginModal', { timeout: 5000 }).should('exist');
+      cy.get('#loginModal', { timeout: 10000 }).should('exist');
   
       // Wait for the modal to have the 'show' class > modal is visible
-      cy.get('#loginModal.show', { timeout: 5000 }).should('exist');
+      cy.get('#loginModal.show', { timeout: 10000 }).should('exist');
   
       // Use '.within' to fill in the login form within the modal
       cy.get('#loginModal').within(() => {
@@ -26,7 +27,7 @@ describe('Login', () => {
       cy.get('#loginForm').submit();
 
       // profile page loads and displays
-      cy.get('#profilePagePrivate', { timeout: 10000 }).should('exist');
+      cy.get('#profilePagePrivate', { timeout: 20000 }).should('exist');
     });
   });
   
