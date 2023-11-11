c918ad2c0260640abbe8c425b4a594b5b926f734
Karen Jo <karenfiksdahl@gmail.com>
2023-11-11 17:07:26 +0100

    updates name on e2e test to include logout test

--- cypress/e2e/integration/login_logout.cy.js
+++ cypress/e2e/integration/login_logout.cy.js
@@ -1,27 +1,33 @@
-describe('Login', () => {
+const validEmail = "karenoroff@noroff.no";
+const validPassword = "karenoroff";
+
+describe('Login and logout', () => {
     it('should log in and access the profile', () => {
       // Visit the login page
       cy.visit('./');
 
       // Delays > minimum of 500ms 
-
       cy.wait(500);      
       
       // Click the login button (modal footer > login > element> data-auth
-      cy.get(".modal-footer button[data-auth='login']").click();
+      cy.get(".modal-footer button[data-auth='login']").click(); 
+      
+      cy.wait(500);
+      // Wait for the loginform to be visible
+      cy.get("#loginForm").should("be.visible");
 
       // Fill in the login form with registered user
-      cy.wait(500);
-      cy.get('#loginEmail').type('karenoroff@noroff.no', { delay: 500 });
+      cy.get('#loginEmail').type(validEmail);
       cy.get('#loginPassword').click();
-      cy.get('#loginPassword').type('karenoroff', { delay: 500 });
+      cy.get('#loginPassword').type(validPassword);
 
       // Submit the form
-      cy.get("#loginModal [data-cy='loginButton']").first().click();
+      cy.get("button[type=submit]").contains("Login").click();
+      cy.wait(2000);
 
       // User redirects to the profile page 
     cy.get('#profilePagePrivate', { timeout: 20000 }).should('exist');
 
     });
   });
-  
+
